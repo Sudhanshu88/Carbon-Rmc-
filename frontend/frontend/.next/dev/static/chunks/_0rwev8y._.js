76e3b880e0ed84742d7e1fb7010564e851494393
Sudhanshu <sudhanshu88@gmail.com>
2026-08-12 11:06:50 +0530

    fix: remove .next from git, update gitignore

--- frontend/frontend/.next/dev/static/chunks/_0rwev8y._.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/app_globals_0yg4wg8.css","static/chunks/node_modules_next_dist_20wefz_._.js","static/chunks/_1iv8q-k._.js","static/chunks/node_modules_next_1mj6ng4._.js","static/chunks/_1-leh6i._.css"],
-    source: "entry"
-});
